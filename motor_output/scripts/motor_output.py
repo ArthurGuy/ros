14cc3bff0d2d469e5d6e2aded0321f3a23c4df85
Arthur Guy <arthur@assys.net>
2018-06-06 21:17:56 +0100

    simplified motor output

--- motor_output/scripts/motor_output.py
+++ motor_output/scripts/motor_output.py
@@ -17,25 +17,17 @@ def get_cmd_vel(data):
     x = data.linear.x
     angular = data.angular.z
     sense.clear([int(data.linear.x), int(data.linear.y), int(data.linear.z)])
-    #self.send_cmd_to_arduino(x, angular)
-
-def get_bearing():
-    dir = sense.get_compass()
-    msg_dir = Float32()
-    msg_dir.data = float(dir)
-    pub_heading.publish(msg_dir)
     
     
-rospy.init_node('car_driver', anonymous=True)
+rospy.init_node('motor_output', anonymous=True)
 rospy.Subscriber('/cmd_vel', Twist, get_cmd_vel)
-pub_heading = rospy.Publisher('~bearing', Float32, queue_size=1)
 
 sense.show_message("Started")
 
 
 while not rospy.is_shutdown():
     try:
-        get_bearing()
+        
     
     except (KeyboardInterrupt, SystemExit):
         raise 
