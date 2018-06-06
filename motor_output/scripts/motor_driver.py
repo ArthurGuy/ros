c5d5296d474b8b0440566a47a8e9b5fed3deb932
Arthur Guy <arthur@arthurguy.co.uk>
2018-06-06 10:46:53 +0100

    testing different output

--- motor_output/scripts/motor_driver.py
+++ motor_output/scripts/motor_driver.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-# license removed for brevity
 import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 import serial
+from sense_hat import SenseHat
 
 
 class driver:
@@ -11,14 +11,19 @@ class driver:
         # init ros
         rospy.init_node('car_driver', anonymous=True)
         rospy.Subscriber('/cmd_vel', Twist, self.get_cmd_vel)
-        self.ser = serial.Serial('/dev/ttyUSB0', 115200)
-        self.get_arduino_message()
+        
+        sense = SenseHat()
+        sense.show_message("Started")
+        
+        #self.ser = serial.Serial('/dev/ttyUSB0', 115200)
+        #self.get_arduino_message()
 
     # get cmd_vel message, and get linear velocity and angular velocity
     def get_cmd_vel(self, data):
         x = data.linear.x
         angular = data.angular.z
-        self.send_cmd_to_arduino(x, angular)
+        sense.clear([x, angular, 0])
+        #self.send_cmd_to_arduino(x, angular)
 
     # translate x, and angular velocity to PWM signal of each wheels, and send to arduino
     def send_cmd_to_arduino(self, x, angular):
