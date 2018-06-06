9d86a176a4f56c6b73d1024a0677dfe73123a0d3
Arthur Guy <arthur@assys.net>
2018-06-06 21:19:31 +0100

    formatting fix

--- motor_output/scripts/motor_output.py
+++ motor_output/scripts/motor_output.py
@@ -13,12 +13,12 @@ sense.clear()
 
 
 def get_cmd_vel(data):
-    //rospy.loginfo(data)
+    #rospy.loginfo(data)
     x = data.linear.x
     angular = data.angular.z
     sense.clear([int(data.linear.x), int(data.linear.y), int(data.linear.z)])
-    
-    
+
+
 rospy.init_node('motor_output', anonymous=True)
 rospy.Subscriber('/cmd_vel', Twist, get_cmd_vel)
 
