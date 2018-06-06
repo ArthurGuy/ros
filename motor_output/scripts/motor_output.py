446b586a5a59ca4b5b2fc0ea4107716569cb2b62
Arthur Guy <arthur@arthurguy.co.uk>
2018-06-06 17:34:14 +0100

    Update motor_output.py

--- motor_output/scripts/motor_output.py
+++ motor_output/scripts/motor_output.py
@@ -10,58 +10,31 @@ sense = SenseHat()
 sense.set_rotation(0)
 sense.clear()
 
+rospy.init_node('car_driver', anonymous=True)
+rospy.Subscriber('/cmd_vel', Twist, get_cmd_vel)
 pub_heading = rospy.Publisher('~bearing', Float32, queue_size=1)
 
-class driver:
-    def __init__(self):
-        # init ros
-        rospy.init_node('car_driver', anonymous=True)
-        rospy.Subscriber('/cmd_vel', Twist, self.get_cmd_vel)
-        
-        sense.show_message("Started")
-        
-        #self.ser = serial.Serial('/dev/ttyUSB0', 115200)
-        #self.get_arduino_message()
-        
-        
-        
-        self.get_bearing()
+sense.show_message("Started")
 
-    # get cmd_vel message, and get linear velocity and angular velocity
-    def get_cmd_vel(self, data):
-        x = data.linear.x
-        angular = data.angular.z
-        sense.clear([x, angular, 0])
-        #self.send_cmd_to_arduino(x, angular)
-        
-    def get_bearing(self):
-        dir = sense.get_compass()
-        msg_dir = Float32()
-        msg_dir.data = float(dir)
-        pub_heading.publish(msg_dir)
+# get cmd_vel message, and get linear velocity and angular velocity
+def get_cmd_vel(data):
+    x = data.linear.x
+    angular = data.angular.z
+    sense.clear([x, angular, 0])
+    #self.send_cmd_to_arduino(x, angular)
 
-    # translate x, and angular velocity to PWM signal of each wheels, and send to arduino
-    def send_cmd_to_arduino(self, x, angular):
-        # calculate right and left wheels' signal
-        right = int((x + angular) * 50)
-        left = int((x - angular) * 50)
-        # format for arduino
-        message = "{},{},{},{},{},{}*".format(right, left, right, left, right, left)
-        print message
-        # send by serial 
-        self.ser.write(message)
+def get_bearing():
+    dir = sense.get_compass()
+    msg_dir = Float32()
+    msg_dir.data = float(dir)
+    pub_heading.publish(msg_dir)
 
-    # receive serial text from arduino and publish it to '/arduino' message
-    def get_arduino_message(self):
-        pub = rospy.Publisher('arduino', String, queue_size=10)
-        r = rospy.Rate(10)
-        while not rospy.is_shutdown():
-            message = self.ser.readline()
-            pub.publish(message)
-            r.sleep()
 
-if __name__ == '__main__':
+while not rospy.is_shutdown():
     try:
-        d = driver()
-    except rospy.ROSInterruptException: 
-        pass
+        get_bearing()
+    
+    except (KeyboardInterrupt, SystemExit):
+        raise 
+    except:
+        traceback.print_exc()
